093ee9b846f811c684b8e047adfcb9b1506337e8
GhislainClermont <43507234+GhislainClermont@users.noreply.github.com>
2021-11-21 18:00:54 -0500

    Download Poll servlets
    - Commented out the old one.
    - Made the new one fetch the currently managed poll and download using the data from query parameters.

--- src/main/java/com/soen387/controller/DownloadPollServlet.java
+++ src/main/java/com/soen387/controller/DownloadPollServlet.java
@@ -13,15 +13,18 @@ public class DownloadPollServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
         PollManager pollManager = getPollManager();
+        String pollId = request.getParameter("pollid");
+        String format = request.getParameter("format");
         response.setDateHeader("Expires", 0);
         PrintWriter out = response.getWriter();
         try {
             StringBuilder fileName = new StringBuilder();
-            pollManager.downloadPollDetails(out, fileName);
+            pollManager.downloadPollDetails(out, fileName, pollManager.getPoll(pollId), format);
             response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
         } catch (Exception e) {
             request.setAttribute("error", e);
-            System.out.println(e);
+            //System.out.println(e);
+            e.printStackTrace();
             request.getRequestDispatcher(Constants.ViewsBaseLink + "error.jsp").forward(request, response);
             return;
         }
