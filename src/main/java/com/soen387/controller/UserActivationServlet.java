a3f55c81ed89939210fdebc2841186f89eaadce6
GhislainClermont <43507234+GhislainClermont@users.noreply.github.com>
2021-12-10 18:20:47 -0500

    UserManagement servlets
    Implemented the servlet code for:
    - Signup
    - User activation
    - Forgot password (1st step)

--- src/main/java/com/soen387/controller/UserActivationServlet.java
+++ src/main/java/com/soen387/controller/UserActivationServlet.java
@@ -1,5 +1,9 @@
 package com.soen387.controller;
 
+import com.soen387.dataaccess.UserBaseFileLoader;
+import com.soen387.usermanager.User;
+import com.soen387.usermanager.UserBase;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,10 +23,29 @@ public class UserActivationServlet extends HttpServlet {
         String username = request.getParameter("username");
         String token = request.getParameter("token");
 
-        //TODO call the business function that will handle the activation action
+        System.out.println(username);
 
-        //if no error redirect to home page
-        response.sendRedirect(request.getContextPath() + "?info=Your account is now activated");
-        return;
+        UserBase userBase = new UserBase(new UserBaseFileLoader());
+        if (token == null) {
+            response.sendRedirect(request.getContextPath() + "?error=Missing token.");
+            return;
+        }
+        User user = userBase.getUserByName(username);
+        if (user == null) {
+            response.sendRedirect(request.getContextPath() + "?error=Account does not exist.");
+            return;
+        } else if (user.isActivated()) {
+            response.sendRedirect(request.getContextPath() + "?error=The account is already activated.");
+            return;
+        }
+
+        if (user.activate(token)) {
+            response.sendRedirect(request.getContextPath() + "?info=Your account has successfully been activated.");
+            userBase.saveUserBase();
+            return;
+        } else {
+            response.sendRedirect(request.getContextPath() + "?error=Could not activate the account. The token does not match");
+            return;
+        }
     }
 }
